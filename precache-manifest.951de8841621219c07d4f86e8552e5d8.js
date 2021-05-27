6b4801a94907662e004a44d21d079eb780c1d59e
Jerome Caruso <jerome0caruso@gmail.com>
2021-05-27 12:42:21 -0500

    Updates

---
+++ precache-manifest.951de8841621219c07d4f86e8552e5d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c8d5098c5fff715b2ae7af9393b1fcd",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "a21d670b54ae56a30586",
+    "url": "/Portfolio/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "874fc96f7a27980d3a9e",
+    "url": "/Portfolio/static/js/2.622422a9.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/Portfolio/static/js/2.622422a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a21d670b54ae56a30586",
+    "url": "/Portfolio/static/js/main.690fac8d.chunk.js"
+  },
+  {
+    "revision": "b4f6ea8748b4b447c1e0",
+    "url": "/Portfolio/static/js/runtime-main.d4d59c43.js"
+  }
+]);
